a6ab1cd38a2921f41c762f94d5a58ce129d2f4d9
André Dargains <andre.dargains@gmail.com>
2021-05-20 21:34:00 +0100

    UPD: login structure;

--- src/screens/Login.vue
+++ src/screens/Login.vue
@@ -17,6 +17,7 @@
           type="text"
         >
       </label>
+      <button>login</button>
     </form>
   </section>
 </template>
@@ -27,8 +28,30 @@ export default {}
 
 <style lang="scss" scoped>
 form {
+  padding: 40px;
+  text-align: center;
+  max-width: 200px;
+  margin: 0 auto;
+  background-color: lightgray;
+  label {
+    display: block;
+    margin-bottom: 16px;
+  }
   span {
     text-transform: uppercase;
+    display: block;
+    text-align: left;
+    font-size: .7em;
+  }
+  button {
+    width: 100%;
+    background-color: black;
+    color: white;
+    padding: 4px 10px;
+    text-transform: uppercase;
+  }
+  input {
+    width: 100%;
   }
 }
 </style>
