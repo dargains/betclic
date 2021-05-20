a6b64855e37c825c309d6fd046db083d213f5699
André Dargains <andre.dargains@gmail.com>
2021-05-20 22:14:04 +0100

    ADD: button component;

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="button"
+    @click="$emit('handle-click')"
+  >
+    <span v-text="label" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    padding: 10px 12px;
+    background-color: #333;
+    width: 100%;
+    span {
+      color: white;
+      font-size: 1rem;
+    text-transform: uppercase;
+    }
+  }
+</style>
