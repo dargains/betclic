d7e03e423b34ecbb6487b2bfc8fcfc65c34f2527
André Dargains <andre.dargains@gmail.com>
2021-05-21 11:30:47 +0100

    UPD: header styling;

--- src/modules/Header.vue
+++ src/modules/Header.vue
@@ -1,15 +1,44 @@
 <template>
   <header class="header">
-    <nav>
-      <a href="#games">Jogos</a>
-      <a href="#news">Notícias</a>
-      <a href="#ranking">Ranking</a>
-    </nav>
+    <div class="wrapper">
+      <nav>
+        <a href="#games">Jogos</a>
+        <a href="#news">Notícias</a>
+        <a href="#ranking">Ranking</a>
+      </nav>
+      <button class="logout">
+        logout
+      </button>
+    </div>
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Header'
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  text-align: center;
+  padding: 20px 0;
+}
+nav {
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+}
+a {
+  text-decoration: none;
+  text-transform: uppercase;
+}
+.logout {
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 5px 10px;
+  background-color: #eee;
+}
+</style>
