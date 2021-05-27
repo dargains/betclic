20d38bbfce6b0415a8c4262a80ecd1e76eecb855
André Dargains <andre.dargains@gmail.com>
2021-05-27 15:42:22 +0100

    ADD: prize module;

---
+++ src/modules/Prize.vue
@@ -0,0 +1,27 @@
+<template>
+  <section
+    id="prize"
+    class="prize"
+  >
+    <div class="wrapper">
+      <figure>
+        <img
+          src="../assets/images/prize.png"
+          alt="Prémio"
+        >
+      </figure>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Prize'
+}
+</script>
+
+<style lang="scss" scoped>
+.prize {
+  margin-top: -40%;
+}
+</style>
